76d853e531bc28fd727bd024006c28b0233674fa
swdean07 <swdean07@gmail.com>
2025-03-06 16:08:57 +0900

    first commit

---
+++ helloworld/helloworld/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, "main.html")
+    # return HttpResponse("안녕하세요. 장고 웹 프레임워크~~ Hello World")
+
+def lunch_list(request):
+    return render(request, "lunch_list.html")
+    # return HttpResponse("lunch_list : 점심 메뉴입니다.")
+
+def introduce(request):
+    return HttpResponse("hello. this is swh.")
+
+def sports(request):
+    return render(request, "sports.html")
+
+def music(request):
+    return render(request, "music.html")
+
+def animal(request):
+    return render(request, "animal.html")
+
+def weather(request):
+    return render(request, "weather.html")
